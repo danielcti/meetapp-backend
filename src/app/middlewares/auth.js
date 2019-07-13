daf72b06b2b16e33f89505bf38ca9b212b02cdc3
danielcti <daniel.cti@hotmail.com>
2019-07-12 21:56:36 -0300

    Initial commit

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+const authConfig = require("../../config/auth");
+
+const auth = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: "Token not provided" });
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+        req.userId = decoded.id;
+    } catch (err) {
+        return res.status(401).json({ error: "Token invalid" });
+    }
+
+    return next();
+};
+
+module.exports = auth;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: "ccca97a60d3dfe78d46cb0f3c4a1dc72",
+    expiresIn: "7d"
+};
